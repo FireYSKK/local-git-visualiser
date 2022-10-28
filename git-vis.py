d12b3726edcd067f6aa8d73b8466b8e7a51c6c12
T-Moore <103999993+FireYSKK@users.noreply.github.com>
2022-10-28 21:58:40 +0300

    Beta release
    Added multiple commits support, hash value of git object is included in dot tree, added different colors for blobs, trees and commits

--- git-vis.py
+++ git-vis.py
@@ -1,13 +1,18 @@
 import binascii
 import zlib
+import os
 import graphviz
+
+repository = input('repository path: ')
 
 
 def read_git_object(git_file: str):
+    global repository
+
     folder = git_file[:2]
     name = git_file[2:]
 
-    filename = f"test/.git/objects/{folder}/{name}"
+    filename = f"{repository}/.git/objects/{folder}/{name}"
 
     with open(filename, "rb") as file:
         raw_data = file.read()
@@ -32,7 +37,8 @@ def read_git_object(git_file: str):
 
 
 def read_heads(heads_name: str):
-    filename = f"test/.git/refs/heads/{heads_name}"
+    global repository
+    filename = f"{repository}/.git/refs/heads/{heads_name}"
 
     with open(filename, "rb") as file:
         data = file.read()
@@ -42,33 +48,67 @@ def read_heads(heads_name: str):
 
 
 dot = graphviz.Digraph("git rep")
-for heads_file in ['master']:
-    content = read_git_object(read_heads(heads_file)[:-1])
-    dot.node(content[1].split('\n')[-2])
+branch = 'master'
+if os.path.exists(repository):
+    last_commit = read_heads(branch)[:-1]
+    content = read_git_object(last_commit)
 
     tree = None
     tree_short_name = "empty"
     parent_name = content[1].split('\n')[-2]
-    if content[0] == 'commit':
-        content_split = content[1].split(maxsplit=1)
+    parent_commit = None
+
+    while content[0] == 'commit':
+        content_split = content[1].split(maxsplit=4)
+
+
+        if parent_commit:
+            dot.node(content[1].split('\n')[-2] + '\n' + parent_commit[:6], fillcolor='orange', style='filled')
+            dot.edge(parent_commit_name + '\n' + last_commit[:6], content[1].split('\n')[-2] + '\n' + parent_commit[:6])
+            parent_name = content[1].split('\n')[-2]
+            last_commit = parent_commit
+        else:
+            dot.node(content[1].split('\n')[-2] + '\n' + last_commit[:6], fillcolor='orange', style='filled')
+
+
         if content_split[0] == 'tree':
-            tree = read_git_object(content_split[1].split('\n', maxsplit=1)[0])
+            tree = read_git_object(content_split[1])
             tree_short_name = content_split[1][:6]
 
-    while (tree) and tree[0] == 'tree':
-        dot.node('Tree '+ tree_short_name)
-        dot.edge(parent_name, 'Tree ' + tree_short_name)
+        parent_commit = None
+        if content_split[2] == 'parent':
+            parent_commit = content_split[3]
+            parent_commit_name = content[1].split('\n')[-2]
 
-        next_tree = None
-        for child in tree[1]:
-            if child[0] == '40000':
-                next_tree = read_git_object(child[2])
-                next_tree_short_name = child[2][:6]
-            elif child[0] == '100644':
-                dot.node(child[1])
-                dot.edge('Tree ' + tree_short_name, child[1])
-        tree = next_tree
-        parent_name = 'Tree ' + tree_short_name
-        tree_short_name = next_tree_short_name
+        has_next_tree = True
+        while has_next_tree:
+            dot.node('Tree ' + tree_short_name, fillcolor='green', style='filled')
+            dot.edge(parent_name + '\n' + last_commit[:6], 'Tree ' + tree_short_name)
 
-print(dot.source)
+            next_tree = None
+            has_next_tree = False
+            for child in tree[1]:
+                if child[0] == '40000':
+                    next_tree = read_git_object(child[2])
+                    next_tree_short_name = child[2][:6]
+                    has_next_tree = True
+                elif child[0] == '100644':
+                    dot.node(child[1] + '\n' + child[2][:6], fillcolor='blue', style='filled')
+                    dot.edge('Tree ' + tree_short_name, child[1] + '\n' + child[2][:6])
+            if has_next_tree:
+                tree = next_tree
+                parent_name = 'Tree ' + tree_short_name
+                tree_short_name = next_tree_short_name
+
+
+        if parent_commit:
+            content = read_git_object(parent_commit)
+        else:
+            content = "empty"
+else:
+    print("Repository doesn't exist")
+
+with open("graph.dot", 'w') as f:
+    f.write(dot.source)
+
+# dot -Tsvg graph.dot > graph.svg
